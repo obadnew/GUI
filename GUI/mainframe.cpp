6310cc21d8e4aae4eb5ddc72fff12fc79a245b94
Obad Zayed <obadzayed@gmail.com>
2024-04-29 22:38:24 +0300

    29/4

--- GUI/mainframe.cpp
+++ GUI/mainframe.cpp
@@ -1,6 +1,7 @@
 #include "mainframe.h"
 #include <wx/wx.h>
 #include <wx/textctrl.h>
+#include <wx/datectrl.h>
 
 
 mainframe::mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
@@ -10,6 +11,34 @@ mainframe::mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 
 	wxStaticText* Task = new wxStaticText(panel, wxID_ANY, "Task Details", wxPoint(25, 25), wxSize(125, 250));
 	wxTextCtrl* readOnlyTextCtrl = new wxTextCtrl(panel, wxID_ANY,
-		"Initial content that can be programmatically changed but not user-edited.",
+		"here where task details will be shown",
 		wxPoint(25, 50), wxSize(300, 250), wxTE_READONLY | wxTE_MULTILINE | wxTE_PASSWORD);
+	wxArrayString choices_1;
+	choices_1.Add("Low");
+	choices_1.Add("Moderate");
+	choices_1.Add("High");
+
+	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(450, 150), wxSize(70, -1), choices_1);
+	choice->Select(0);
+	wxStaticText* Task_Priority = new wxStaticText(panel, wxID_ANY, "Priority", wxPoint(450, 135), wxSize(125, 250));
+
+
+
+	wxArrayString choices_2;
+	choices_2.Add("College");
+	choices_2.Add("Work");
+	choices_2.Add("House");
+	choices_2.Add("Family");
+	choices_2.Add("Hobbies");
+	choices_2.Add("Friends");
+	choices_2.Add("Personal Time");
+
+	wxChoice* choice_T = new wxChoice(panel, wxID_ANY, wxPoint(450, 200), wxSize(120, -1), choices_2);
+	choice_T->Select(0);
+	wxStaticText* Task_Type = new wxStaticText(panel, wxID_ANY, "Category", wxPoint(450, 185), wxSize(125, 250));
+
+
+
+	wxDatePickerCtrl* datePicker = new wxDatePickerCtrl(panel, wxID_ANY, wxDefaultDateTime, wxPoint(450, 250), wxSize(150, -1), wxDP_DEFAULT | wxDP_DROPDOWN);
+	wxStaticText* Task_Date = new wxStaticText(panel, wxID_ANY, "Date", wxPoint(450, 235), wxSize(125, 250));
 }
